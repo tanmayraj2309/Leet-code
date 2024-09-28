9e03cabf990921debf6271e3936fb00ad68f3bd5
Tanmay Raj <118921083+tanmayraj2309@users.noreply.github.com>
2024-09-28 18:23:35 +0530

    Time: 0 ms (100%), Space: 42 MB (78%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,5 +1,34 @@
 class Solution {
     public void rotate(int[][] matrix) {
+
+        // transpose // j sirf i tk isliye kyon ki half part niche wla ko hi swap kr rhe
+        for(int i=1;i<matrix[0].length;i++){
+            for(int j=0;j<i;j++)
+            {
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+                
+            }
+        }
+
+
+        // Reverse the array Row WWWise
+        
+        
+        for(int i=0;i<matrix[0].length;i++){
+            int s=0;
+            int e=matrix[0].length-1;
+            while(s<e){
+                int temp=matrix[i][s];
+                matrix[i][s]=matrix[i][e];
+                matrix[i][e]=temp;
+                e--;
+                s++;
+
+            }
+        }
+       
         
     }
 }
