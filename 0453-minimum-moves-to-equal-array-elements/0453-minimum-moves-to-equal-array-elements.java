ddbd18215f4bd6ba759f032858c37744d0737186
Tanmay Raj <118921083+tanmayraj2309@users.noreply.github.com>
2024-10-04 23:55:24 +0530

    Time: 13 ms (13.23%), Space: 45.5 MB (70.72%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.java
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(nums);
         int res=0;
         for(int i=0;i<nums.length;i++){
-            int a=-1*(nums[0]-nums[i]);
+            int a=Math.abs(nums[0]-nums[i]);
             res+=a;
             
         }
