59366d5fbbca7dccd4e57421400e7687e5a01838
Tanmay Raj <118921083+tanmayraj2309@users.noreply.github.com>
2024-10-09 17:36:24 +0530

    Time: 2 ms (26.85%), Space: 41.7 MB (16.86%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(!st.isEmpty()&&st.peek()=='('&& ch==')'){
+                st.pop();
+            }
+            else{
+            st.push(ch);
+            }
+        }
+        return st.size();
+    }
+}
