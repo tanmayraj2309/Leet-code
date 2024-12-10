cb14ddf8fca95742333aa8ed61dd9936622a22b7
Tanmay Raj <118921083+tanmayraj2309@users.noreply.github.com>
2024-12-10 23:55:19 +0530

    Time: 0 ms (100%), Space: 44.3 MB (58.02%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -13,14 +13,32 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
 
         // Approach 1:  Using HashSet
-        ListNode curr= head;
-        Set<ListNode> set= new HashSet<>();
-        while(curr!=null){
-            if(set.contains(curr)) return true;
-            set.add(curr);
-            curr=curr.next;
-        }
-        return false;
+        // ListNode curr= head;
+        // Set<ListNode> set= new HashSet<>();
+        // while(curr!=null){
+        //     if(set.contains(curr)) return true;
+        //     set.add(curr);
+        //     curr=curr.next;
+        // }
+        // return false;
+
+
+        // Aprroach 2 =  Floyd Hare and Tortoise Algorithm
         
+        if(head==null){
+            return false;
+        }
+        ListNode slow=head;
+        ListNode fast = head.next;
+        while(slow!=fast){
+          
+          if(fast== null || fast.next==null){
+            return false;
+          }
+           slow=slow.next;
+           fast=fast.next.next;
+        }
+        return true;
+  
     }
 }
