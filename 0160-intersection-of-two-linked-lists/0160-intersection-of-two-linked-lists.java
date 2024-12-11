0c610be52eff264e1bd0b1900ef9e70fabdc274d
Tanmay Raj <118921083+tanmayraj2309@users.noreply.github.com>
2024-12-11 21:39:15 +0530

    Time: 1 ms (99.9%), Space: 48.5 MB (52.37%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -13,22 +13,54 @@ public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
         // APRROACH 1 :  Brute force tc: o(m*n)
-        ListNode currA=headA;
-        ListNode currB=headB;
+        // ListNode currA=headA;
+        // ListNode currB=headB;
 
-        while(currA!=null){
+        // while(currA!=null){
             // har baar jb iterate hoke aayega curr a ke 
             //sth check krne ke baad tb phir se head b assign kr dege
-            currB=headB;
-            while(currB!=null){
-                if(currA==currB){
-                    return currA;
-                }
-                currB=currB.next;
-            }
-            currA=currA.next;
-        }
-        return null;
+        //     currB=headB;
+        //     while(currB!=null){
+        //         if(currA==currB){
+        //             return currA;
+        //         }
+        //         currB=currB.next;
+        //     }
+        //     currA=currA.next;
+        // }
+        // return null;
+
+
+      
+      // Aprroach 2 : HASHSET APPROACH
+    //    Set<ListNode> set = new HashSet<>();
+    //    ListNode currA=headA,
+    //    currB=headB;
+
+    //    while(currA!=null){
+    //     set.add(currA);
+    //     currA=currA.next;
+    //    }
+
+    //    while(currB!=null){
+    //     if(set.contains(currB)){
+    //         return currB;
+    //     }
+    //     currB=currB.next;
+    //    }
+
+    //     return null;
+
+
+    // Approach 3 : Two Poninter   // dist= travel of a =x+y+z; travel of b=z+y+x// similar as 3+2=5; 2+3=5
+     ListNode currA=headA,
+      currB=headB;
+      while(currA!=currB){
+        currA=currA==null ?headB: currA.next;
+        currB= currB==null ? headA : currB.next;
+      }
+      return currA;
+
         
     }
 }
