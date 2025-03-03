4dc7d8fdeb7524f42ae1eb053fa04c050b89669c
Tanmay Raj <118921083+tanmayraj2309@users.noreply.github.com>
2025-03-03 15:44:29 +0530

    Time: 0 ms (100%), Space: 42.2 MB (7.65%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -11,23 +11,48 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
          
-         // Aprroach 1 in 1 iteration
-        // make sentinel node 
-        ListNode phead= new ListNode(0);
-        phead.next=head;
-        ListNode temp=phead;
-        ListNode curr= head;
-        int len=0;
-        while(curr!=null){
-            len++;
-            curr=curr.next;
-        }
-        int diff=len-n;
-        for(int i=0;i<diff;i++){
+         // Aprroach 1 in 2  iteration Brute Force 
+
+         ListNode temp=head;
+         int count=0;
+         while(temp!=null){
+            count++;
             temp=temp.next;
-        }
-        temp.next=temp.next.next;
-         return phead.next;
+         }
+         if(n==count){
+            head=head.next;
+            return head;
+         }
+         int len=count-n+1;
+
+         count=1;
+         temp=head;
+
+         while(temp!=null){
+            count++;
+            if(count==len){
+                break;
+            }
+            temp=temp.next;
+         }
+         temp.next=temp.next.next;
+         return head;
+        // make sentinel node 
+        // ListNode phead= new ListNode(0);
+        // phead.next=head;
+        // ListNode temp=phead;
+        // ListNode curr= head;
+        // int len=0;
+        // while(curr!=null){
+        //     len++;
+        //     curr=curr.next;
+        // }
+        // int diff=len-n;
+        // for(int i=0;i<diff;i++){
+        //     temp=temp.next;
+        // }
+        // temp.next=temp.next.next;
+        //  return phead.next;
 
         
     }
