80fc1b9de03dd488ec2043aecf61c8ad4da67891
Tanmay Raj <118921083+tanmayraj2309@users.noreply.github.com>
2025-03-03 16:09:41 +0530

    Time: 0 ms (100%), Space: 41.6 MB (86.91%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -13,46 +13,52 @@ class Solution {
          
          // Aprroach 1 in 2  iteration Brute Force 
 
-         ListNode temp=head;
-         int count=0;
-         while(temp!=null){
-            count++;
-            temp=temp.next;
-         }
-         if(n==count){
-            head=head.next;
-            return head;
-         }
-         int len=count-n+1;
-
-         count=1;
-         temp=head;
-
-         while(temp!=null){
-            count++;
-            if(count==len){
-                break;
-            }
-            temp=temp.next;
-         }
-         temp.next=temp.next.next;
-         return head;
-        // make sentinel node 
-        // ListNode phead= new ListNode(0);
-        // phead.next=head;
-        // ListNode temp=phead;
-        // ListNode curr= head;
-        // int len=0;
-        // while(curr!=null){
-        //     len++;
-        //     curr=curr.next;
-        // }
-        // int diff=len-n;
-        // for(int i=0;i<diff;i++){
+        //  ListNode temp=head;
+        //  int count=0;
+        //  while(temp!=null){
+        //     count++;
         //     temp=temp.next;
-        // }
-        // temp.next=temp.next.next;
-        //  return phead.next;
+        //  }
+        //  if(n==count){
+        //     head=head.next;
+        //     return head;
+        //  }
+        //  int len=count-n+1;
+
+        //  count=1;
+        //  temp=head;
+
+        //  while(temp!=null){
+        //     count++;
+        //     if(count==len){
+        //         break;
+        //     }
+        //     temp=temp.next;
+        //  }
+        //  temp.next=temp.next.next;
+        //  return head;
+
+
+
+        // 2nd approach in one iteration  simply we take two pointer slow and fast 
+        // make fast traverse till n then incement  slow and fast by 1,1 and till fast.next!=null
+        // automaticatically by calculation slow pointer just 1 place ahead of deleting desired node
+
+
+        ListNode slow=head;
+        ListNode fast=head;
+
+        for(int i=0;i<n;i++){
+            fast=fast.next;
+        }
+        if(fast==null) return head.next;
+
+        while(fast.next!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        slow.next=slow.next.next;
+        return head;
 
         
     }
