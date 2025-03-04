164da0941b375dd2349321f60f4e05eb6758d1c3
Tanmay Raj <118921083+tanmayraj2309@users.noreply.github.com>
2025-03-04 19:15:52 +0530

    Time: 1 ms (99.91%), Space: 48.3 MB (79.7%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -52,58 +52,68 @@ public class Solution {
     //     return null;
 
 
-    // Approach 3 : Two Poninter   // dist= travel of a =x+y+z; travel of b=z+y+x// similar as 3+2=5; 2+3=5
-    //  ListNode currA=headA,
-    //   currB=headB;
-    //   while(currA!=currB){
-    //     currA=currA==null ?headB: currA.next;
-    //     currB= currB==null ? headA : currB.next;
-    //   }
-    //   return currA;
 
 
-      /// Approach 4 : simply we find the length of both the linked list 
+
+      /// Approach 3 : simply we find the length of both the linked list 
       // after that jisk large length - small length krgee 
       // after that jo big length list usko wha se traverse krna suru krege jo 
       //bhi subtaraction of length ka result aaya hai then check krege b ke sath 
       //whether they will meet or not
 
-      ListNode currA = headA;
-      ListNode currB= headB;
-      int lenA=0;
-      int lenB=0;
-      while(currA!=null){
-        lenA++;
-        currA=currA.next;
-      }
-      while(currB!=null){
-        lenB++;
-        currB=currB.next;
-      }
-      currA=headA;
-      currB=headB;
-      int diff=Math.abs(lenA-lenB);
+    //   ListNode currA = headA;
+    //   ListNode currB= headB;
+    //   int lenA=0;
+    //   int lenB=0;
+    //   while(currA!=null){
+    //     lenA++;
+    //     currA=currA.next;
+    //   }
+    //   while(currB!=null){
+    //     lenB++;
+    //     currB=currB.next;
+    //   }
+    //   currA=headA;
+    //   currB=headB;
+    //   int diff=Math.abs(lenA-lenB);
 
-      if(lenA>lenB){
-        int i=0;
-        while(i<diff){
-            currA=currA.next;
-            i++;
-        }
-      }
-      else{
-        int i=0;
-        while(i<diff){
-            currB=currB.next;
-            i++;
-        }
-      }
+    //   if(lenA>lenB){
+    //     int i=0;
+    //     while(i<diff){
+    //         currA=currA.next;
+    //         i++;
+    //     }
+    //   }
+    //   else{
+    //     int i=0;
+    //     while(i<diff){
+    //         currB=currB.next;
+    //         i++;
+    //     }
+    //   }
 
-      while(currA!=currB){
-        currA=currA.next;
-        currB=currB.next;
-      }
-      return currA;
+    //   while(currA!=currB){
+    //     currA=currA.next;
+    //     currB=currB.next;
+    //   }
+    //   return currA;
+
+
+
+
+
+        // Approach 3 : Two Poninter   // dist= travel of a =x+y+z; travel of b=z+y+x// similar as 3+2=5; 2+3=5
+        //    if(headA==null || headB==null) return null;
+           ListNode temp1=headA;
+           ListNode temp2=headB;
+           while(temp1!=temp2){
+            temp1=temp1.next;
+            temp2=temp2.next;
+            if(temp1==temp2) return temp1;
+            if(temp1==null) temp1=headB;  /// cross head pe jayega
+            if(temp2==null) temp2=headA;  /// cross head pe jayega
+           }
+           return temp1;
         
     }
 }
