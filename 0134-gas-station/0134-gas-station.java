aac45147a04f4635ca9caa9586f659acdedfa2e0
Tanmay Raj <118921083+tanmayraj2309@users.noreply.github.com>
2025-12-16 23:54:57 +0530

    Time: 2 ms (99.01%), Space: 119.5 MB (12.63%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        // int max = Integer.MAX_VALUE;
+        // int i = 0;
+        // int cost = 0;
+        
+        // int minI = -1;
+        // int minValue = Integer.MAX_VALUE;
+        // while(i < gas.length){
+        //             if(cost[i]-gas[i]<0) continue;
+
+        //     int diff = gas[i] - cost[i];
+        //     minValue = Math.max(minValue,diff);
+        //     minI = i;
+        //     if(diff == minValue){
+        //         minI = Math.min(minI,i);
+        //     }
+        // }
+        // int n = gas.length;
+        // for(i = minI;i<n)
+        int sum1=0;
+        int sum2 =0;
+        for(int i=0;i<gas.length;i++){
+          sum1+=gas[i];
+          sum2+=cost[i];
+        }
+        if(sum2>sum1) return -1;
+
+        int start=0;
+        int tank=0;
+        for(int i=0;i<gas.length;i++){
+          // if(cost[i]-gas[i]<0) continue;
+           
+            tank+=gas[i]-cost[i];
+            if(tank<0 ){
+                start=i+1;
+                tank=0;
+            }
+            // else if(tank>=0){
+            //     start=i;
+            // }
+            // else{
+            //     return start;
+            // }
+            
+        }
+        return start;
+    }
+}
